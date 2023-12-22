6dbd23b9775c31cca6b1a33dd826b20d62c0639e
Din <din14000@gmail.com>
2023-12-22 21:16:22 +0200

    ok

--- app/Data/detailsData.js
+++ app/Data/detailsData.js
@@ -1,4 +1,4 @@
-{
+export const detailsData = {
   "detailsData": [
     {
       "detailsSubStep": 1,
--- app/steps/details-step/detailsUtils/DetailsUtils.tsx
+++ app/steps/details-step/detailsUtils/DetailsUtils.tsx
@@ -1,5 +1,5 @@
-import detailsData from '@/app/data/detailsData.json';
-import {detailsDataType } from "@/app/types/detailsDataType";
+import { detailsData } from '@/app/data/detailsData';
+import { detailsDataType } from "@/app/types/detailsDataType";
 
 export const detailsDataObject: detailsDataType = {
     detailsData: detailsData.detailsData
