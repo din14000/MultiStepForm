bf0245cdd332ac49c54fd952c7765299122c376c
Din <din14000@gmail.com>
2023-12-22 21:18:16 +0200

    ok

--- app/steps/details-step/detailsUtils/DetailsUtils.tsx
+++ app/steps/details-step/detailsUtils/DetailsUtils.tsx
@@ -1,5 +1,5 @@
-import { detailsData } from '@/app/data/detailsData';
-import { detailsDataType } from "@/app/types/detailsDataType";
+import { detailsData } from '@/app/Data/detailsData';
+import {detailsDataType } from "@/app/types/detailsDataType";
 
 export const detailsDataObject: detailsDataType = {
     detailsData: detailsData.detailsData
